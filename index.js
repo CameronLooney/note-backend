77d4c5d7de3b67c693a2df30375ba24a8180e696
Cameron <cameronlooney18@gmail.com>
2023-05-11 23:39:34 +0100

    added frontend

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 app.use(express.json())
+app.use(express.static('build'))
 let morgan = require('morgan')
 app.use(morgan('tiny'))
 const cors = require('cors')
